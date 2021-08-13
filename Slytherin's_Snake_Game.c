807170bb50fb87fc7f07e7a3c59ab74403e44f0a
Kashish Bataju <kashishbataju999@gmail.com>
2021-08-13 16:01:08 +0545

    Widened the border

--- Slytherin's_Snake_Game.c
+++ Slytherin's_Snake_Game.c
@@ -7,7 +7,7 @@
 #include <time.h>  
 #include <ctype.h>
 
-int i, j, height = 20, width = 20;
+int i, j, height = 30, width = 60;
 int gameover, score;
 int x, y, fruitx, fruity, flag;
   
@@ -21,11 +21,11 @@ void setup()
     x = height / 2;
     y = width / 2;
 label1:
-    fruitx = rand() % 20;
+    fruitx = rand() % 30;
     if (fruitx == 0)
         goto label1;
 label2:
-    fruity = rand() % 20;
+    fruity = rand() % 60;
     if (fruity == 0)
         goto label2;
     score = 0;
@@ -37,16 +37,15 @@ void draw()
     system("cls");
     for (i = 0; i < height; i++) {
         for (j = 0; j < width; j++) {
-            if (i == 0 || i == width - 1
+            if (i == 0 || i == height - 1
                 || j == 0
-                || j == height - 1) {
+                || j == width - 1) {
                 printf("#");
             }
             else {
                 if (i == x && j == y)
                     printf("0");
-                else if (i == fruitx
-                         && j == fruity)
+                else if (i == fruitx && j == fruity)
                     printf("*");
                 else
                     printf(" ");
