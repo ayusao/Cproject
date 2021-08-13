87a91e3a9814b46818f728574e0e4564d7f832bd
unknown <077bct036.khagendra@pcampus.edu.np>
2021-08-13 14:13:53 +0545

    Recording of scores added

---
+++ Slytherin's_Snake_Game.c
@@ -0,0 +1,191 @@
+// C program to build the complete
+// snake game
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>  
+
+int i, j, height = 20, width = 20;
+int gameover, score;
+int x, y, fruitx, fruity, flag;
+  
+// Function to generate the fruit
+// within the boundary
+void setup()
+{
+    gameover = 0;
+  
+    // Stores height and width
+    x = height / 2;
+    y = width / 2;
+label1:
+    fruitx = rand() % 20;
+    if (fruitx == 0)
+        goto label1;
+label2:
+    fruity = rand() % 20;
+    if (fruity == 0)
+        goto label2;
+    score = 0;
+}
+  
+// Function to draw the boundaries
+void draw()
+{
+    system("cls");
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
+            if (i == 0 || i == width - 1
+                || j == 0
+                || j == height - 1) {
+                printf("#");
+            }
+            else {
+                if (i == x && j == y)
+                    printf("0");
+                else if (i == fruitx
+                         && j == fruity)
+                    printf("*");
+                else
+                    printf(" ");
+            }
+        }
+        printf("\n");
+    }
+  
+    // Print the score after the
+    // game ends
+    printf("score = %d", score);
+    printf("\n");
+    printf("press X to quit the game");
+}
+  
+// Function to take the input
+void input()
+{
+    if (kbhit()) {
+        switch (getch()) {
+        case 'a':
+            flag = 1;
+            break;
+        case 's':
+            flag = 2;
+            break;
+        case 'd':
+            flag = 3;
+            break;
+        case 'w':
+            flag = 4;
+            break;
+        case 'x':
+            gameover = 1;
+            break;
+        }
+    }
+}
+  
+// Function for the logic behind
+// each movement
+void logic()
+{
+    sleep(0.01);
+    switch (flag) {
+    case 1:
+        y--;
+        break;
+    case 2:
+        x++;
+        break;
+    case 3:
+        y++;
+        break;
+    case 4:
+        x--;
+        break;
+    default:
+        break;
+    }
+  
+    // If the game is over
+    if (x < 0 || x > height
+        || y < 0 || y > width)
+        gameover = 1;
+  
+    // If snake reaches the fruit
+    // then update the score
+    if (x == fruitx && y == fruity) {
+    label3:
+        fruitx = rand() % 20;
+        if (fruitx == 0)
+            goto label3;
+  
+    // After eating the above fruit
+    // generate new fruit
+    label4:
+        fruity = rand() % 20;
+        if (fruity == 0)
+            goto label4;
+        score += 10;
+    }
+}
+void record(){
+   char plname[20],nplname[20],cha,c;
+   int i,j,px;
+   FILE *info;
+   info=fopen("record.txt","a+");
+   getch();
+   system("cls");
+   printf("Enter your name\n");
+   scanf("%[^\n]",plname);
+   //************************
+   for(j=0;plname[j]!='\0';j++){ //to convert the first letter after space to capital
+   nplname[0]=toupper(plname[0]);
+   if(plname[j-1]==' '){
+   nplname[j]=toupper(plname[j]);
+   nplname[j-1]=plname[j-1];}
+   else nplname[j]=plname[j];
+   }
+   nplname[j]='\0';
+   //*****************************
+   //sdfprintf(info,"\t\t\tPlayers List\n");
+   fprintf(info,"Player Name :%s\n",nplname);
+    //for date and time
+ 
+   time_t mytime;
+  mytime = time(NULL);
+  fprintf(info,"Played Date:%s",ctime(&mytime));//**************************
+     fprintf(info,"Score:%d\n",px=score);//write the scores in the file
+   for(i=0;i<=50;i++)
+   fprintf(info,"%c",'_');
+   fprintf(info,"\n");
+   fclose(info);
+   printf("wanna see past records press 'y'\n");
+   cha=getch();
+   system("cls");
+   if(cha=='y'){
+   info=fopen("record.txt","r");
+   do{
+       putchar(c=getc(info));
+       }while(c!=EOF);}
+     fclose(info);
+}
+  
+// Driver Code
+void main()
+{
+    int m, n;
+  
+    // Generate boundary
+    setup();
+  
+    // Until the game is over
+    while (!gameover) {
+  
+        // Function Call
+        draw();
+        input();
+        logic();
+    }
+    record();
+}
